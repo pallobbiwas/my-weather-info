9595b652d31ee8f23ad327eaf4a5576992dd0f3d
pallobbiswas <pallobbiswas326@gmail.com>
2022-03-08 22:54:57 +0600

    ses

--- weather.js
+++ weather.js
@@ -1,5 +1,7 @@
 /* select button and click handler */
 const buttonClickHandeler = () => {
+    const detailsontainer = document.getElementById('display-container')
+    detailsontainer.textContent = ''
   const searchInput = document.getElementById("search-input");
   const searchInputValue = searchInput.value;
   if (!searchInputValue) {
@@ -18,8 +20,38 @@ const getSearch = (name) => {
 };
 // display data
 const display = (datas) => {
-    const displayData = document.getElementById('display-container');
+    showDetailes(datas.next_days);
+    // console.log(datas.next_days);
+  const displayAfter = document.getElementById("display-afer");
+  displayAfter.innerHTML = `
+    <div class="text-center py-5 box">
+            <h1>Name: ${datas.region}</h1>
+            <h2 class="">teamparatur : ${datas.currentConditions.temp.c} deg</h2>
+            <img class="w-25 img-fluid image bg-info" src="${datas.currentConditions.iconURL}" alt="">
+            <h3 class=" py-3">Status : ${datas.currentConditions.comment}</h3>
+        </div>
+    `;
     
+};
+const showDetailes = (values) => {
+    const detailsontainer = document.getElementById('display-container')
+    for(const value of values){
+        console.log(value);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card text-center">
+                <h3>name: ${value.day}</h3>
+                <div class="text-center">
+                    <img src="${value.iconURL}" class="w-50 img-fluid " alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">max-tem: ${value.max_temp.c}</h5>
+                    <h5 class="card-text">min-tam: ${value.min_temp.c}</h5>
+                </div>
+            </div>
+        `;
+        detailsontainer.appendChild(div);
 
+    }
 }
-
